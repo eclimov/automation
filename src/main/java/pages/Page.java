b00163e1ce8fc5e3050599757347b71efc178b36
eduard.climov <select * from dbo.item_barcodes ib inner join Sage_ET..item_prices ip on ib.sage_item_id = ip.item_id and ip.sales_interface_id = 57 where ib.sale_date is null and ib.expiration_date is not null>
2018-06-07 17:38:27 +0300

    - Added useful methods to parent "Page" class. Not implemented yet.

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -2,8 +2,9 @@ package pages;
 
 import initialization.Initialization;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.getElement;
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class Page extends Initialization {
 
@@ -16,4 +17,46 @@ public class Page extends Initialization {
         $("input[id*='pass'], input[name*='pass'], input[class*='pass']").sendKeys(password);
         $("form[id*='login'], form[name*='login'], form[class*='login']").submit();
     }
+
+    //TODO: adapt the methods below
+    /*
+    protected int getRandomOptionIndex(Select select, int minIndex) {
+        int min = minIndex;
+        int optionsCount = select.getOptions().size();
+        if (optionsCount > min) {
+            int max = optionsCount - 1;
+            int randomOption = getRandomInt(min, max);
+            write("Select option #" + randomOption + " from: " + optionsCount + " options");
+            return randomOption;
+        } else {
+            String fileName = "note";
+            //String newFileName = makeScreenshot(fileName, (WebElement) select);
+            //write("<br><a href='" + newFileName + "' target='_blank'><img src='" + newFileName + "' style='max-width: 500px;'/></a><br><b>Select '<i>" + select.toString() + "</i>' doesn't have options to choose from");
+            fail("Select '<i>" + select.toString() + "</i>' doesn't have options to choose from. ");
+            return -1;
+        }
+    }
+
+    public void randomSelect(Select select) {
+        int min = 0;
+        if (select.getFirstSelectedOption().getAttribute("value").equals("0")) {
+            min++;
+        }
+        select.selectByIndex(getRandomOptionIndex(select, min));
+    }
+
+    protected List<WebElement> findElements(By by) {
+        return getDriver().findElements(by);
+    }
+
+    public void randomRadio(WebElement radio) {
+        List<WebElement> radios = findElements("//input[@type='radio'][@name='" + radio.getAttribute("name") + "']");
+        radios.get(getRandomInt(0, radios.size() - 1)).click();
+    }
+
+    protected void alert_accept() {
+        Alert alert = getDriver().switchTo().alert();
+        alert.accept();
+    }
+    */
 }
